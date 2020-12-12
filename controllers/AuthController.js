cf770609a62d2b9b1d78f947773fc126548876be
Mevlüt EFE <67636958+efemevlut@users.noreply.github.com>
2020-12-12 19:47:41 +0300

    Add express validator

--- controllers/AuthController.js
+++ controllers/AuthController.js
@@ -1,29 +1,46 @@
 const User = require("../models/UserModel");
+const bcrypt = require("bcrypt");
+const { validationResult } = require("express-validator");
 
 exports.authRegister = async (req, res) => {
-    
-    const {firstName, lastName, email, password} = req.body;
-    
-    //TODO1: validate the fields
-    //TODO2: check already registered
-    //TODO3: crpyt password
-    //TODO4: save the user DB
+  const { firstName, lastName, email, password } = req.body;
 
-    const user = new User ({
-        firstName,
-        lastName,
-        email,
-        password // crypted password
-    });
-    
-    await user.save();
+  //TODO1: validate the fields
 
-    // TODO: Error handling for saving
-    res.send("Register Completed")
-}
+  
+  const validationErr = validationResult(req);
+  console.log("validationErr", validationErr);
+
+  if(validationErr.errors.length > 0){
+      return res.status(400).json({errors: validationErr.array()})
+  }
+  const userData = await User.findOne({ email });
+
+  if (userData) {
+    return res
+      .status(400)
+      .json({ errors: [{ message: "User already exists!!" }] });
+  }
+
+  const salt = await bcrypt.genSalt(10);
+
+  const newPassword = await bcrypt.hash(password, salt);
+
+  const user = new User({
+    firstName,
+    lastName,
+    email,
+    password: newPassword, // crypted password
+  });
+
+  await user.save();
+
+  // TODO: Error handling for saving
+  res.send("Register Completed");
+};
 
 exports.authLogin = (req, res) => {
-    //TODO Auth.
-    //TODO Login func
-    res.send("Login completed")
-}
+  //TODO Auth.
+  //TODO Login func
+  res.send("Login completed");
+};
--- models/connectDb.js
+++ models/connectDb.js
@@ -1,11 +1,16 @@
 const mongoose = require("mongoose");
 
-const connectDB = async () =>{
-    try {
-        await mongoose.connect(process.env.MONGODB_URI, {useNewUrlParser: true, useUnifiedTopology: true});
-        console.log("Successfully connected DB")
-    } catch (error) {
-        console.log("Error connecting DB", error)
-    }
-}
+const connectDB = async () => {
+  try {
+    await mongoose.connect(process.env.MONGODB_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useFindAndModify: false,
+      useCreateIndex: true,
+    });
+    console.log("Successfully connected DB");
+  } catch (error) {
+    console.log("Error connecting DB", error);
+  }
+};
 module.exports = connectDB;
